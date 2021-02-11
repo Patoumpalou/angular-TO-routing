b88c55995573159de136fa347d77d2d1530fc7d4
Patoumpalou <pierrehouviez@gmail.com>
2021-02-11 22:58:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-e1rvbm-v4qma3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-e1rvbm-v4qma3)
---
+++ src/app/crisis-center/crisis-center/crisis-center.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-crisis-center",
+  templateUrl: "./crisis-center.component.html",
+  styleUrls: ["./crisis-center.component.css"]
+})
+export class CrisisCenterComponent implements OnInit {
+  ngOnInit() {
+    console.log("center");
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
